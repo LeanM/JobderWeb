a1b6f345012fa3a342fe22bd0e410b27c5e748d7
LeanM <42581931+LeanM@users.noreply.github.com>
2024-02-12 23:27:44 -0300

    fixed not refreshing state 2

---
+++ src/components/chat-components/ChatUsers.js
@@ -0,0 +1,69 @@
+import { createUseStyles } from "react-jss";
+import ChatUserItem from "./ChatUserItem";
+import { colors } from "../../assets/colors";
+import { forwardRef } from "react";
+import { useImperativeHandle } from "react";
+
+const ChatUsers = forwardRef((props, ref) => {
+  const { chatRoomUsers, actualRecipientId } = props;
+
+  const classes = useStyles();
+
+  useImperativeHandle(ref, () => ({
+    moveChatroomUserToFrontOnMessage,
+  }));
+
+  const moveChatroomUserToFrontOnMessage = (userId) => {
+    let chatroomUserToMove = null;
+    let newChatRoomUsersList = [];
+
+    const chatroomUsersArray = [...chatRoomUsers];
+
+    chatroomUsersArray.every((chatroomUser) => {
+      if (chatroomUser.user.id === userId) {
+        chatroomUserToMove = chatroomUser;
+        return true;
+      } else {
+        newChatRoomUsersList.push(chatroomUser);
+        return true;
+      }
+    });
+
+    if (chatroomUserToMove != null) {
+      chatroomUserToMove.chatRoom.state = "UNSEEN";
+      return [chatroomUserToMove, ...newChatRoomUsersList];
+    } else return false;
+  };
+
+  return (
+    <div className={classes.chatUsersContainer}>
+      {chatRoomUsers.map((chatroomUser) => {
+        return (
+          <ChatUserItem
+            key={chatroomUser.interaction.id}
+            onSelect={(userId) => props.onSelect(userId)}
+            onChange={() => props.onReset()}
+            chatroomUserData={chatroomUser}
+            actualRecipientId={actualRecipientId}
+          />
+        );
+      })}
+    </div>
+  );
+});
+
+export default ChatUsers;
+
+const useStyles = createUseStyles({
+  chatUsersContainer: {
+    width: "30%",
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    borderRight: `solid 1px ${colors.primary}`,
+    gap: "1rem",
+    paddingTop: "2rem",
+  },
+});
