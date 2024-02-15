43ffafe90b560c47e0668eaa9cc5bef942807583
LeanM <42581931+LeanM@users.noreply.github.com>
2024-02-15 01:36:11 -0300

    changes

--- src/components/chat-components/ChatScreen.js
+++ src/components/chat-components/ChatScreen.js
@@ -108,7 +108,6 @@ export default function ChatScreen() {
         message.senderId
       );
       if (newUserOrder) {
-        console.log("ASD");
         setChatRoomUsers(newUserOrder);
       } else getChatRoomUsers();
     }
