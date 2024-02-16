43f62bb41f531fd225ebbbcffd695c70bab2be63
LeanM <42581931+LeanM@users.noreply.github.com>
2024-02-15 23:10:24 -0300

    cleanup in idb context

--- src/context/ChatIndexDBProvider.js
+++ src/context/ChatIndexDBProvider.js
@@ -1,12 +1,9 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useEffect } from "react";
 import { openDB } from "idb";
 
 const ChatIndexDBContext = createContext({});
 
 export const ChatIndexDBProvider = ({ children }) => {
-  const [chats, setChats] = useState([]);
-  const [currentChat, setCurrentChat] = useState({});
-
   useEffect(() => {
     async function fetchChats() {
       const db = await openDB("user-chats", 1, {
@@ -16,13 +13,7 @@ export const ChatIndexDBProvider = ({ children }) => {
           });
         },
       });
-
-      const tx = db.transaction("chats", "readonly");
-      const store = tx.objectStore("chats");
-      const chats = await store.getAll();
-      setChats(chats);
     }
-
     fetchChats();
   }, []);
 
@@ -31,7 +22,6 @@ export const ChatIndexDBProvider = ({ children }) => {
     const tx = db.transaction("chats", "readonly");
     const store = tx.objectStore("chats");
     const chat = await store.get(userChatId);
-    //const chat = chats.find((chat) => chat.userChatId === userChatId);
     return chat;
   };
 
@@ -40,67 +30,33 @@ export const ChatIndexDBProvider = ({ children }) => {
     newChatStatus,
     messages
   ) => {
-    console.log("---------");
     const db = await openDB("user-chats", 1);
     const tx = db.transaction("chats", "readwrite");
     const store = tx.objectStore("chats");
     const chat = await store.get(userChatId);
-
-    console.log(chat);
     let updatedChat;
     let updatedChatMessages;
     if (chat) {
-      console.log("EXISTE");
       updatedChatMessages = [...chat.chatMessages].concat(messages);
       updatedChat = {
         ...chat,
         userChatStatus: newChatStatus,
         chatMessages: updatedChatMessages,
       };
-      console.log("UPDATED");
-      console.log(updatedChat);
-      /*
-      setChats((prev) => {
-        return prev.map((chat) => {
-          if (chat.userChatId !== userChatId) return chat;
-          else {
-            return updatedChat;
-          }
-        });
-      });
-      */
     } else {
-      console.log("NO EXISTE");
       updatedChatMessages = messages;
       updatedChat = {
         userChatId: userChatId,
         userChatStatus: newChatStatus,
         chatMessages: updatedChatMessages,
       };
-      //setChats((prev) => [...prev, updatedChat]);
     }
-
     store.put(updatedChat);
-    console.log("---------");
-  };
-
-  useEffect(() => {
-    //updateIndexDB();
-  }, [chats]);
-
-  const updateIndexDB = async () => {
-    const db = await openDB("user-chats", 1);
-    const tx = db.transaction("chats", "readwrite");
-    const store = tx.objectStore("chats");
-    chats.map((chat) => {
-      store.put(chat);
-    });
   };
 
   return (
     <ChatIndexDBContext.Provider
       value={{
-        currentChat,
         handleSelectChat,
         handleAddMessagesToChat,
       }}
