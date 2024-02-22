2f5592abd07bf90b2bce05b9e0637c1d3e0098dc
LeanM <42581931+LeanM@users.noreply.github.com>
2024-02-22 17:13:30 -0300

    small fix

--- src/components/chat-components/chatUsersComponents/worker/SolicitantItem.js
+++ src/components/chat-components/chatUsersComponents/worker/SolicitantItem.js
@@ -64,6 +64,7 @@ export default function SolicitantItem(props) {
         }}
         onAccept={() => {
           updateDataStatusOnAccept();
+          setOpenUserInteraction(false);
         }}
       />
 
